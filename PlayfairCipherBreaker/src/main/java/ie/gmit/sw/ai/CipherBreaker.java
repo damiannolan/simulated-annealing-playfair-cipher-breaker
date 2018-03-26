64c46bb332e4a57178aeb1320a9c2276fe47cd47
Damian Nolan <damiannolan@gmail.com>
2018-03-26 02:22:03 +0100

    added main algorithm from test runner to SimulatedAnnealing class

--- PlayfairCipherBreaker/src/main/java/ie/gmit/sw/ai/CipherBreaker.java
+++ PlayfairCipherBreaker/src/main/java/ie/gmit/sw/ai/CipherBreaker.java
@@ -1,79 +1,11 @@
 package ie.gmit.sw.ai;
 
-import java.security.SecureRandom;
-import java.util.Random;
-
-import ie.gmit.sw.ai.cipher.PlayfairCipher;
-import ie.gmit.sw.ai.keys.IKeyGenerator;
-import ie.gmit.sw.ai.keys.KeyGenerator;
-import ie.gmit.sw.ai.keys.KeyShuffler;
-import ie.gmit.sw.ai.simulated_annealing.FitnessCalculator;
+import ie.gmit.sw.ai.ui.UserInterface;
 
 public class CipherBreaker {
-	
-    public static void main( String[] args ) {
-    	long time = System.currentTimeMillis();
-    	// Add a file parser for reading in cipherText
-    	String cipherText = "HFZQLYVEDWNITIQPQDUVHYLGXZHFNYBKPACAZQHFVQIQCUUVYCBXABQZQZURHQDZHBKDMVZQHXRGURLQHTXZQVDFYXZHRGGWHBYEGXNYYEGKYVHFLQDBWDVQIZEAUCAHHPQIBRRVBREZNYYQAHPUQDUVHYZXGNRDEOZWQFKCLZZHXVRDEOFEINQZZKZPKDYDCAMEEQUDBCLDBKPAEDUVYCHFZQQEUMSVPBUMURLQHTXZXZCUHTVTPHMDLDRGMDLDVBHCMGUVYCQVPVDMSZXQCPDIQZLQKDUBEMTCYDDBCQGDFEUKQZVPCYUHKDIABDFVFEETGKIDOZEFURLQUVYCKDPTACYQUCFUPVVBBREZZXDTZPWCMEDILYTHZHADMUDBGQHBKIFEMDEWIZRGVQHTKCNWIEGNHCPLLUDPCOFTQGDPNWBYHCHFQZITQVGKUVYCHFBDQVHVHCHFDIYXHFBRUMLZKDZDFQFHNYLGSAPLQCCAZQHCPCBODITCVBMUHFDIYXHFBRUMLZKDLULIDLIDDLQRKWZQACYQUZBHZBDUBHQZUKUZEDGWTVBXABQZQZBUFEUFFTQVEKZQINAHMEPTDFNYFBIZEXBRRVBREZTCILEVFBEDHUBRWDLYTHFHIZNYCPOVBDLIZQHFQPQDUVHYLGCUNYOKDMPCHTXZPCGCHFDYLQDBLTHPQEKCGKTIQIBRVQHBQNDBRXBZEFRFVUEDQYNYMZCPBDHYLKCUXF";
-    	
-    	Random rand = new SecureRandom();
-
-    	IKeyGenerator keygen = KeyGenerator.getInstance();
-    	PlayfairCipher pf = new PlayfairCipher(cipherText);
-    	FitnessCalculator fitness = new FitnessCalculator();
-    	
-    	String parent = keygen.generateKey();
-    	
-    	//String[] digrams = pf.createDigrams(cipherText);
-    	//String decrypted = pf.decrypt(digrams, parent);
-    	
-    	String decrypted = pf.decrypt(parent);
-
-    	double parentScore = fitness.logProbability(decrypted);
-       	double bestScore = parentScore;
-       	    	
-    	for(int temp = 20; temp > 0; temp = temp - 1) {
-    		
-    		for(int trans = 50000; trans > 0; trans = trans - 1) {
-    			
-    			String childKey = KeyShuffler.shuffleKey(parent);
-    			
-    			decrypted = pf.decrypt(childKey);
-    			//decrypted = pf.decrypt(digrams, childKey);
-    			
-    			double childScore = fitness.logProbability(decrypted);
-
-    			double dF = childScore - parentScore;
-    			
-    			if(dF > 0) {
-    				parentScore = childScore;
-    				parent = childKey;
-    			} else {    				
-    				double prob = (Math.exp((dF / temp)));
-
-    				if(prob > rand.nextDouble()) {
-    					parentScore = childScore;
-    					parent = childKey;
-    				}
-    			}
-    			
-    			if(parentScore > bestScore) {
-    				bestScore = parentScore;
-    				String bestKey = parent;
-    				System.out.printf("\nBest Key: %s With Score: %.5f\n", bestKey, bestScore);
-    		    	System.out.println("Decrypted Message: " + decrypted);
-    		    	System.out.printf("With Temperature: %d and Transitions: %d\n", temp, trans);;
-    			}
-    			
-    		} // end transitions
-    		
-    		if(parentScore == bestScore) {
-    			break;
-    		}
-    		
-    	} // end temperature
-    	
-        long end = System.currentTimeMillis() - time;
-        System.out.println(end + "ms");
-    }
+	public static void main(String[] args) {
+		UserInterface ui = new UserInterface();
+		
+		ui.start();
+	}
 }
--- PlayfairCipherBreaker/src/main/java/ie/gmit/sw/ai/simulated_annealing/SimulatedAnnealing.java
+++ PlayfairCipherBreaker/src/main/java/ie/gmit/sw/ai/simulated_annealing/SimulatedAnnealing.java
@@ -1,15 +1,21 @@
 package ie.gmit.sw.ai.simulated_annealing;
 
+import java.security.SecureRandom;
+import java.util.Random;
+
 import ie.gmit.sw.ai.cipher.PlayfairCipher;
 import ie.gmit.sw.ai.keys.IKeyGenerator;
 import ie.gmit.sw.ai.keys.KeyGenerator;
+import ie.gmit.sw.ai.keys.KeyShuffler;
 
 public class SimulatedAnnealing {
-	private FitnessCalculator fitness;
+	// probably don't need this
+	// if PlayfairCipher contains the parentKey access via getKey()
 	private IKeyGenerator keygen;
 	private PlayfairCipher cipher;
+	private FitnessCalculator fitness;
 	private int temp;
-	
+
 	public SimulatedAnnealing(PlayfairCipher cipher, int temp) {
 		super();
 		this.fitness = new FitnessCalculator();
@@ -17,10 +23,55 @@ public class SimulatedAnnealing {
 		this.cipher = cipher;
 		this.temp = temp;
 	}
-	
+
 	public void start() {
 		/*
 		 * Bulk of algorithm goes here
 		 */
+		Random rand = new SecureRandom();
+		String parentKey = keygen.generateKey();
+		String decipheredText = cipher.decrypt(parentKey);
+
+		double parentScore = fitness.logProbability(decipheredText);
+		double bestScore = parentScore;
+
+		for (int temp = this.temp; temp > 0; temp = temp - 1) {
+			for (int trans = 50000; trans > 0; trans = trans - 1) {
+				String childKey = KeyShuffler.shuffleKey(parentKey);
+				decipheredText = cipher.decrypt(childKey);
+				double childScore = fitness.logProbability(decipheredText);
+
+				double dF = childScore - parentScore;
+
+				if (dF > 0) {
+					parentScore = childScore;
+					parentKey = childKey;
+				} else {
+					double prob = (Math.exp((dF / temp)));
+
+					if (prob > rand.nextDouble()) {
+						parentScore = childScore;
+						parentKey = childKey;
+					}
+				}
+
+				if (parentScore > bestScore) {
+					bestScore = parentScore;
+					String bestKey = parentKey;
+					displayStats(bestKey, bestScore, decipheredText, temp, trans);
+				}
+
+			} // end transitions
+			
+			if (parentScore == bestScore) {
+				break;
+			}
+		} // end temperature
+	}
+	
+	public void displayStats(String bestKey, double bestScore, String decipheredText, int temp, int trans) {
+		System.out.printf("\nBest Key: %s With Score: %.5f\n", bestKey, bestScore);
+		System.out.println("Decrypted Message: " + decipheredText);
+		System.out.printf("With Temperature: %d and Transitions: %d\n", temp, trans);
 	}
 }
